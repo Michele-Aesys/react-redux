7bccf26aea5afc819f10f4fae4ee77e31d591cbd
Michele-Aesys <85109451+Michele-Aesys@users.noreply.github.com>
2021-07-05 11:53:11 +0200

    fix loading in ShowCat.jsx

--- src/components/showCat.jsx
+++ src/components/showCat.jsx
@@ -4,14 +4,14 @@ import { fetchCat } from '../features/catSlice'
 const ShowCat = ()=>{
 const {cats,loading} = useSelector(state=>state.cat)
 const dispatch = useDispatch()
-console.log(cats,loading);
     return <div>
             <div>
                 <button onClick={()=>dispatch(fetchCat())}>Show Cat</button>
                 </div>
+                <div>{loading}</div>
             {cats.length>0?cats.map((elem,index)=>{
                 return <img key={index} src={elem[0].url}></img>
-                    }):<div>{loading}</div>}
+                    }):<div></div>}
     </div>
 }
 export default ShowCat
